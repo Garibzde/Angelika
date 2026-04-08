5770f2d03ef6d225e48abf7606b815696e5823e7
Garibzde <q.qaribzade@gmail.com>
2026-04-08 14:00:11 +0400

    added style page

---
+++ assets/js/fancy.js
@@ -0,0 +1,12 @@
+Fancybox.bind('[data-fancybox="gallery"]', {
+    Thumbs: {
+      autoStart: true,
+    },
+    Toolbar: {
+      display: {
+        left: [],
+        middle: [],
+        right: ["zoom", "slideshow", "fullscreen", "close"],
+      },
+    },
+  });
